b3d536c7afb5722a96d70b2c2432c76298d49cde
Your Name <ashutoshgautam055@gmail.com>
2025-09-20 12:15:00 +0530

    Initial commit

--- AreaCalculator.js
+++
@@ -1,9 +0,0 @@
-function calculateTriangleArea(base, height) {
-    let area = (base * height) / 2;
-    console.log(`The area of the triangle is: ${area}`);
-}
-
-calculateTriangleArea(10, 5)
-calculateTriangleArea(0, 15)
-calculateTriangleArea(8, 0)
-calculateTriangleArea(7, 9)
--- SimpleInterestCalculator.js
+++
@@ -1,7 +0,0 @@
-function calculateSimpleInterest(principal, rate, time) {
-    let interest = (principal * rate * time) / 100;
-    console.log(`The simple interest is: ${interest.toFixed(1)}`);
-}
-calculateSimpleInterest(1000, 5, 3)
-calculateSimpleInterest(1500, 7, 5)
-calculateSimpleInterest(0, 6, 2)
